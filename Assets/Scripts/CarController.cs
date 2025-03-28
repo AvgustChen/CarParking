8460954bfce3aa9c8ad0cb6f48d63bebdbb4b4a8
Avgust Chen <avchen@yandex.ru>
2025-03-28 21:34:09 +0300

    first commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,76 @@
+using System;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] private float speed = 5f, finalSpeed = 15f, rotationSpeed = 50f;
+    private bool isClicked;
+    [NonSerialized] public Vector3 finalPosition;
+
+    private float curPointX, curPointY;
+
+    private enum Axis
+    {
+        vertical, horizontal
+    }
+
+    [SerializeField] private Axis carAxis;
+
+    private enum Direction
+    {
+        Right, Left, Top, Bottom, None
+    }
+    private Direction carDirectionX = Direction.None, carDirectionY = Direction.None;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnMouseDown()
+    {
+        curPointX = Input.mousePosition.x;
+        curPointY = Input.mousePosition.y;
+    }
+
+    private void OnMouseUp()
+    {
+        isClicked = true;
+        if (Input.mousePosition.x - curPointX > 0 && carAxis == Axis.horizontal)
+            carDirectionX = Direction.Right;
+        else if(Input.mousePosition.x - curPointX < 0 && carAxis == Axis.horizontal)
+            carDirectionX = Direction.Left;
+
+        if (Input.mousePosition.y - curPointY > 0 && carAxis == Axis.vertical)
+            carDirectionY = Direction.Top;
+        else if (Input.mousePosition.y - curPointY < 0 && carAxis == Axis.vertical)
+            carDirectionY = Direction.Bottom;
+    }
+
+    private void Update()
+    {
+        if (finalPosition != Vector3.zero)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, finalPosition, finalSpeed * Time.deltaTime);
+
+            Vector3 lookAtPos = finalPosition - transform.position;
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, Quaternion.LookRotation(lookAtPos), rotationSpeed * Time.deltaTime);
+        }
+
+        if (transform.position == finalPosition)
+            Destroy(gameObject);
+    }
+
+    private void FixedUpdate()
+    {
+        if (isClicked && finalPosition == Vector3.zero)
+        {
+            Vector3 whichWay = carAxis == Axis.horizontal ? Vector3.forward : Vector3.left;
+            speed = Math.Abs(speed);
+            if (carDirectionX == Direction.Left || carDirectionY == Direction.Bottom)
+                speed *= -1;
+            rb.MovePosition(rb.position + whichWay * speed * Time.fixedDeltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/RoadFinishTrigger.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class RoadFinishTrigger : MonoBehaviour
+{
+    [SerializeField] private Vector3 finalPosition;
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Car"))
+        {
+            other.GetComponent<CarController>().finalPosition = finalPosition;
+        }
+    }
+}
